d1cf5af6f7bf55368ef78c281d7a1bff45359650
afsana43 <afsanahamidmim70@gmail.com>
2024-09-04 21:59:54 +0600

    learning loop

---
+++ for-of-loop.js
@@ -0,0 +1,12 @@
+const names = ['mimi', 'mini', 'kharap manush'];
+for (const name of names) {
+    console.log(name);
+    
+}
+
+
+const numbers = [5, 6, 7, 78];
+for (const number of numbers){
+    console.log(number);
+    
+}
---
+++ while.js
@@ -0,0 +1,15 @@
+// structure while loop:
+let number = 1;
+while( number <= 10){
+    console.log(number);
+    number ++;
+    
+
+}
+
+
+// for loop:
+for(let num=1; num < 7; num++){
+    console.log(num);
+    
+}
