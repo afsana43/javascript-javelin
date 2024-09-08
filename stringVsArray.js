a1a903656c09b152d6681d0876b72c41a515c9fd
afsana43 <afsanahamidmim70@gmail.com>
2024-09-08 17:31:13 +0600

    string done

---
+++ stringVsArray.js
@@ -0,0 +1,42 @@
+// string declare
+
+let name = 'm im';
+// console.log( name);
+
+
+// string length check
+// console.log(name.length);
+
+// type of check
+// console.log(typeof name);
+
+// index set
+name [1] = 'M';
+// console.log( name);
+
+// string to convert object
+let names = new String('mehedy');
+console.log(names);
+console.log( typeof names);
+
+
+
+
+
+
+// array declare:
+let nums = [23, 34, 35, 56]
+// console.log(nums);
+
+// array length checking
+// console.log(nums.length);
+
+// typeOf check
+// console.log(typeof nums);
+
+// index set
+
+nums[2]=200;
+// console.log(nums);
+
+
