f6d12553e683010480c0b4a7775c966f9dc09c4e
afsana43 <afsanahamidmim70@gmail.com>
2024-10-02 15:11:50 +0600

    dynamic string

---
+++ stringType.js
@@ -0,0 +1,15 @@
+
+// normal string type:
+
+const firstName = 'Mimi';
+const secondName = 'hamid'
+const fullName = firstName +" "+ secondName;
+console.log(fullName);
+ 
+
+// dynamic template string:
+
+const firstName2 = 'Mimi';
+const secondName2 = 'hamid'
+const fullName2 =`Hi My name is ${firstName2} ${secondName2}`;
+console.log(fullName2);
